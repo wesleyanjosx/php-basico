348aadb0812cc3b2e9672afdd2e9c40b9ee5efb7
wesleyanjosx <wesleyanjosprime@gmail.com>
2026-09-06 19:52:59 -0300

    update - aula05 operadores aritmeticos

---
+++ aula05/operadores.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 05 PHP</title>
+</head>
+<body>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+
+        echo 'A soma vale '. $n1 + $n2.'<br>';
+        echo 'A subtração vale '. $n1 -$n2.'<br>';
+        echo 'A multiplicação vale '. $n1 * $n2.'<br>';
+        echo 'A divisão vale '. $n1 / $n2.'<br>';
+        echo 'A modulo vale '. $n1 % $n2.'<br>';
+    ?>
+</body>
+</html>
