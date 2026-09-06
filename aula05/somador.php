0da7844b6de0a7019346b4f12753a834b0596f7e
wesleyanjosx <wesleyanjosprime@gmail.com>
2026-09-06 19:47:27 -0300

    add - aula05 operadores aritmeticos

---
+++ aula05/somador.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 05 PHP</title>
+</head>
+<body>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        $s = $n1 + $n2;
+        echo 'A soma de '. $n1 .' e '. $n2 .' é igual a '. $s .'<br>';
+    ?>
+</body>
+</html>
