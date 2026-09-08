ca3855320e39ecc3997b58d7682998ba3e698d14
wesleyanjosx <wesleyanjosprime@gmail.com>
2026-09-08 16:31:06 -0300

    update - aula06 referencia

---
+++ aula06/referencia.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP básico</title>
+</head>
+<body>
+    <?php
+        $a = 3;
+        $b = $a;
+        $b += 5;
+        echo 'A variável <b>A</b> vale '. $a .'<br>';
+        echo 'A variável <b>B</b> vale '. $b .'<br>';
+    ?>
+</body>
+</html> 
